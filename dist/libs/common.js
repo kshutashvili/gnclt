7753358493439721ea68ceae8687b1bdb17a3b5f
ruslan201010@gmail.com <ruslan201010@gmail.com>
2018-03-10 22:58:46 +0200

    gnc push

---
+++ dist/libs/common.js
@@ -0,0 +1,72 @@
+window.onload = function() {
+	var popup = new Popup();
+	$('.btn-play').on('click', function() {
+		console.log(1)
+		popup.open($('.popup #site-video'));
+	});
+
+	var countDownDate = new Date("Mar 13, 2018 0:00:00").getTime();
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+    // Output the result in an element with id="demo"
+    function pretty_time_string(num) {
+    	return ( num < 10 ? "0" : "" ) + num;
+    }
+    
+    $('#days').text(pretty_time_string(days));
+    $('#hours').text(pretty_time_string(hours));
+    $('#minutes').text(pretty_time_string(minutes));
+    $('#seconds').text(pretty_time_string(seconds));
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+    	clearInterval(x);
+    	document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+};
+
+function Popup() {
+	var popup = $('.popup');
+	var self = this;
+	var popupFade = 200;
+	var contentFade = 200;
+
+	self.open = function(content) {
+		self.content = content;
+		popup.fadeIn(popupFade);	
+		content.fadeIn(contentFade);
+	}
+
+	self.close = function(e) {
+		var targ = e.target;
+
+		if (!targ.classList.contains('popup') 
+			&& !targ.classList.contains('popup__close')) return;
+			$('.popup-content').fadeOut(contentFade);
+		popup.fadeOut(popupFade);
+	}
+
+	self.changeContent = function(changeEl) {
+		self.content.fadeOut(contentFade, function() {
+			changeEl.fadeIn(contentFade);
+		});
+	}
+
+	popup.on('click', self.close);
+}
